ba875b2ca025eb40b16c53184a088f95ca380a5f
Navin Soni <navinsoni89@gmail.com>
2018-06-10 22:51:43 -0400

    added register to client and server

---
+++ client/src/components/Register.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+  <h1>Register</h1>
+  <input
+    type='email'
+    name='email'
+    placeholder='email'
+    v-model='email'
+  />
+  <br>
+  <input
+      type='password'
+      name='password'
+      placeholder='password'
+      v-model=password
+  />
+  <br>
+  <button @click=register>
+  Register
+  </button>
+  </div>
+</template>
+
+
+<script>
+import authenticationService from '@/services/AuthenticationService'
+export default {
+methods:{
+    async register(){
+      const res = await authenticationService.register({
+        'email':this.email,
+        'password':this.password
+      })
+      console.log('clicked on register', this.email, this.password)
+      console.log(res) 
+    }
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,13 @@ app.use(cors())
 
 app.get('/status', (req, res) => {
   res.send({
-    message: 'hello world'
+    message: 'hello world!'
+  })
+})
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}! Your email has been registered! Have fun!`
   })
 })
 
